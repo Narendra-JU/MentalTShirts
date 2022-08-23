64365c828ca531ac0d6cf9b6f30a614ce04d09d8
personal <mortalexcalibar@gmail.com>
2022-08-24 02:27:04 +0530

    notification channel done

--- app/src/main/java/com/example/mentalapp/ui/MentalSplashScreen.kt
+++ app/src/main/java/com/example/mentalapp/ui/MentalSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.example.mentalapp.ui
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -69,6 +72,8 @@ class MentalSplashScreen : ComponentActivity() {
 
 
                 }
+
+                createNotificationChannel()
             }
         }
     }
@@ -79,6 +84,12 @@ class MentalSplashScreen : ComponentActivity() {
             val channelId = getString(R.string.notification_channel_id)
             val name = getString(R.string.notification_channel_name)
             val descriptionText = getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name , importance).apply {
+                description = descriptionText
+            }
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
